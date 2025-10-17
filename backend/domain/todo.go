89b955f67bb2f2db94d1e09d574f806ea7be71aa
asa-cording <asahiwork.cording@gmail.com>
2025-10-17 21:43:50 +0900

    Todoモデルをdomainへ移動

--- backend/domain/todo.go
+++ backend/domain/todo.go
@@ -1,1 +1,8 @@
 package domain
+
+// Todoモデル
+type Todo struct {
+	id        uint   `gorm: primaryKey; autoIncrement`
+	titie     string `gorm: type:string; not null`
+	completed bool   `gorm: type:boolean; not null; default:false`
+}
